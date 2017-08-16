91c6ccd80f53bd31d819801b12ec001836a9392b
Tibbers <xuchang@gznb.com>
2017-08-16 09:06:10 +0800

    'ddd'

--- LinqExpressionTest/Program.cs
+++ LinqExpressionTest/Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Linq;
-using LinqExpressionTest.LinqProvider;
+using System.Text;
+using LinqExpressionTest.Provider.MySql;
 using LinqExpressionTest.Models;
+using LinqExpressionTest.Provider.Sqlite;
+
+//using Microsoft.Data.Sqlite;
+
+
 
 namespace LinqExpressionTest
 {
@@ -9,15 +15,41 @@ namespace LinqExpressionTest
     {
         static void Main(string[] args)
         {
-            var res = new SqlQueryable<Student>()
-                .Where(x => x.Gender == "男"&&x.Id==1)
-                .Select(x=>new
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var db = new SqliteDb("Data Source=linqTest;");
+            InitSqliteDataBase(db);
+            var list = db.CreateQuery<Student>()
+                 .Where(x => x.Name == "张三")
+                .Select(x => new
                 {
                     x.Gender,
                     x.Name
                 })
-                .ToList();
+                 .ToList();
+            list.ForEach(s => Console.WriteLine($"Id:{s.Id}  Name:{s.Name}  Gender:{s.Gender}"));
             Console.Read();
+        }
+
+        private static void InitSqliteDataBase(IDb db)
+        {
+
+            var res = db.ExecuteScalar<int>("SELECT COUNT(*) FROM sqlite_master where type='table' and name='Student'");
+            if (res == 0)
+            {
+                db.Execute(@" CREATE TABLE Student(
+   Id INTEGER  PRIMARY KEY,
+   Name           TEXT    NOT NULL,
+   Gender         TEXT     NOT NULL
+);");
+                db.Execute("insert into Student(Name,Gender) values('张三','男')");
+                db.Execute("insert into Student(Name,Gender) values('李四','男')");
+                db.Execute("insert into Student(Name,Gender) values('王五','男')");
+                db.Execute("insert into Student(Name,Gender) values('张三','女')");
+                db.Execute("insert into Student(Name,Gender) values('张三','男')");
+            }
+
+
         }
     }
 }
